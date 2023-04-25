3b33dc2aaaf91e6a2fd57e8da44b18371a181e8c
Dohyeon Kwon <playdev7@gmail.com>
2023-04-25 22:51:49 +0900

    Python PS - 1402 Problem Solved. 간단하게 생각하면 정말 쉬움.

--- Python/1402.py
+++ Python/1402.py
@@ -1,36 +1,18 @@
 # https://solved.ac/profile/playdev7
-# 2023-04-14 시간초과
 
-# 입력받은 수를 인수분해 및 덧셈하는 함수 선언
-def factorization(num):
-    factors = []
-    sum_factors = 0
+# 문제 a의 "정수인" 약수를 더해서 b로 만들 수 있으면 "yes"를 출력하는 프로그램.
+# 정답 - 뭘 해도 yes가 나옴..
+# 2에서 4 -> 2*1*1 -> 2+1+1 = 4
+# 11에서 0 -> 11*-1*-1*-1*-1*-1*-1*-1*-1*-1*-1*-1* +1*-1 -> 11-1-1-1-1-1-1-1-1-1-1-1 +1-1 == 11-12+1 -> 0
+# 따라서 테스트 케이스만큼 입력 받고 전부 yes 출력
 
-    for i in range(1, num+1):
-        # num의 약수면 약수 리스트에 추가
-        if num % i == 0:
-            factors.append(i)
-            # num을 약수로 나눔
-            num = num / i
-    
-    # 인수들에 대해 덧셈 진행.
-    for i in factors:
-        sum_factors += i
-    
-    # 인수들을 덧셈한 값을 반환
-    return sum_factors
+test_case = int(input())
 
+result = []
 
-def main():
-    test_case = int(input())
-    for i in range(test_case):
-        a, b = map(int, input().split())
-        
-        # 인수들을 덧셈한 값이 B와 일치하면 "yes" 출력
-        if factorization(a) == b:
-            print("yes")
-        else:
-            print("no")
+for i in range(test_case):
+    a, b = map(int, input().split())
+    result.append("yes")
 
-if __name__ == "__main__":
-    main()
+for i in result:
+    print(i)
