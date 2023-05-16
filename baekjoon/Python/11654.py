dcd6b2385c8d1ebf3e52f166de14a1a9a290f623
Dohyeon Kwon <playdev7@gmail.com>
2023-05-16 19:08:31 +0900

    Python PS - 11654 Problem Solved.

---
+++ baekjoon/Python/11654.py
@@ -0,0 +1,10 @@
+# https://solved.ac/profile/playdev7
+# 11654 - 아스키 코드
+
+# 문제 - 알파벳 대문자, 소문자, 숫자 문자열이 입력되면 해당하는 ASCII 코드를 출력하는 프로그램.
+
+# ord() 메소드 사용하여 빠르게 풀이.
+
+char = input()
+
+print(ord(char))
