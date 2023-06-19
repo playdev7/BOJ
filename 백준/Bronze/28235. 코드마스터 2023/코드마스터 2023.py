7e6f09f97d6868ee4a66c77aa5146032249626e9
Dohyeon Kwon <111621564+playdev7@users.noreply.github.com>
2023-06-19 19:07:11 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 34412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,16 @@
+# https://solved.ac/profile/playdev7
+# 송도고 코드마스터 2023 - A 번
+# 구호 SONGDO에 대해 HIGHSCHOOL로 응원.
+# 구호 CODE에 대해 MASTER로 응원.
+# 구호 2023에 대해 0611로 응원.
+# 구호 ALGORITHM에 대해 CONTEST로 응원
+
+구호 = input()
+if 구호 == "SONGDO":
+    print("HIGHSCHOOL")
+if 구호 == "CODE":
+    print("MASTER")
+if 구호 == "2023":
+    print("0611")
+if 구호 == "ALGORITHM":
+    print("CONTEST")
